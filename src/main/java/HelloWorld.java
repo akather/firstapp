8c864ca09820ad5cf82310b7c53a5735e7cd3789
Cloud User <centos@ip-172-31-88-61.ec2.internal>
2019-07-21 16:40:14 +0000

    All new files for simple web application

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/* Simple Hello world application - Abdul Kather.
+**
+*/
+
+public class HelloWorld 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World - Rise and Shine with Abdul from JAVA !!");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<!-- Simple JSP file by Abdul Kather -->
+<html>
+<body>
+<h1>Hello World - Very Good morning from JSP by Abdul Kather !!</h1>
+</body>
+</html>
